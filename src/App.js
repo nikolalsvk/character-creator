18a95dc242835e63d40e18e32a53f590bd3880e1
Nikola Đuza <nikolaseap@gmail.com>
2021-04-13 11:14:42 +0200

    Finish exercise 4 - perspective decoration with isolation

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
-import React from 'react';
+import React from "react";
 
-import CharacterEditor from './components/CharacterEditor';
-import Footer from './components/Footer';
+import CharacterEditor from "./components/CharacterEditor";
+import Footer from "./components/Footer";
+import PerspectiveDecoration from "./components/PerspectiveDecoration";
 
 function App() {
   return (
     <>
+      <PerspectiveDecoration />
       <CharacterEditor />
       <Footer />
     </>
---
+++ src/components/PerspectiveDecoration/PerspectiveDecoration.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+import styles from "./PerspectiveDecoration.module.css";
+
+const PerspectiveDecoration = () => (
+  <div className={`${styles.perspectiveDecoration}`} />
+);
+
+export default PerspectiveDecoration;
